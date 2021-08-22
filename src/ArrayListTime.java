21e92a5e63615ab2e3b854caa4500066db606dad
Sameony <iam2001sam@gmail.com>
2021-08-22 14:57:47 +0530

    Modified ArrayLIst intro

---
+++ src/ArrayListTime.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayListTime {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> first = new ArrayList <Integer>(1);
+		first.add(10);
+		first.add(20);
+		System.out.println(first);
+	}
+
+	
+}
