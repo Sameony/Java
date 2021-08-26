bfaa6c42a01eb922f9003cfa20cd58b5980a7e88
Sameony <iam2001sam@gmail.com>
2021-08-27 00:13:49 +0530

    Traversal Techniques Initialised

---
+++ src/Traversals.java
@@ -0,0 +1,24 @@
+public class Traversals {
+
+	public static void main(String[] args) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		root.left.right = new Node(50);
+		root.right.left = new Node(60);
+		root.right.right = new Node(70);
+		root.left.left.left = new Node(80);
+		root.left.left.right = new Node(90);
+		root.left.right.left = new Node(100);
+		root.left.right.right = new Node(110);
+		root.right.left.left = new Node(120);
+		root.right.left.right = new Node(130);
+		root.right.right.left = new Node(140);
+		root.right.right.right = new Node(150);
+		
+		
+	}
+	
+
+}
